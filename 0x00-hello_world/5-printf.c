f0059b9cabfbffe0111fc1025d0dfabe5ac75eed
Lauryn05 <waruingilauryn05@gmail.com>
2023-02-16 13:01:56 -0800

    printfshow

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-
+/*
+ * main -Entry
+ * Return: 0 always
+ */
 int printf(const char *format, ...);
 
 int main(void)
 {
-	printf("%s%s%s\n", "with proper grammar, ", "but the outcome is a piece of art", "");
-	return 0;
+	printf("%s%s%s\n", "with proper grammar, ", "but the outcome is a piece of art", " ");
+	return (0);
 }
