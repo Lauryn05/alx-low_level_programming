b88d61bf0b12b7320d9e15b4644eb89ad363b38e
Lauryn05 <waruingilauryn05@gmail.com>
2023-02-17 12:22:06 -0800

    printchar prac

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *  main - Entry point
+ *
+ *  Return: 0 Always
+ */
+int main(){
+	char ch;
+
+	for(ch = 'a';ch <= 'z';ch++) {
+		if(ch != e && ch != q){
+			printchar(ch);
+		}
+	}
+printchar('\n');
+
+return (0);
+}
