513d578d02ceb274317ede26658f60525138edb3
Lauryn05 <waruingilauryn05@gmail.com>
2023-02-17 16:53:02 -0800

    61011

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,19 +2,19 @@
 /**
  * main -Entry point
  *
- * Rreturn: Always 0
+ * Return: Always 0
  */
 int main(void)
 {
-       	int i, j;
+	int i, j;
 	
 	for (i = 0; i < 9; i++)
 	{
 		for (j = i + 1; j < 10; j++)
-        {
-	       	putchar(i + '0');
-		putchar(j + '0');
-		if (i != 8 || j != 9)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i != 8 || j != 9)
 		{
 			putchar(',');
 			putchar(' ');
