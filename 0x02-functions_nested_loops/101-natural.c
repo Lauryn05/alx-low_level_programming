f694755770aaeeac4417cc5196689b5049dbc8c7
Lauryn05 <waruingilauryn05@gmail.com>
2023-02-22 10:59:47 -0800

    nat

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints sum of multiples of 3 or 5 up to 1024
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x, y=0;
+
+	while (x < 1024)
+	{
+		if ((x % 3 == 0) || (x % 5 == 0))
+		{
+			y += x;
+		}
+		x++;
+	}
+	printf("%d\n", y);
+	return (0);
+}		
