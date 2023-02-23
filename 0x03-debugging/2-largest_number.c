4451b78ed530f7fca0c0720bace2ab6016d7b307
Lauryn05 <waruingilauryn05@gmail.com>
2023-02-23 06:00:52 -0800

    all

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,13 +8,13 @@
  */
 int largest_number(int a, int b, int c)
 {
-       	int largest;
+	int largest;
 
-	if (a > b && b > c)
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b > a && b > c)
 	{
 		largest = b;
 	}
@@ -22,6 +22,5 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-
 	return (largest);
 }
