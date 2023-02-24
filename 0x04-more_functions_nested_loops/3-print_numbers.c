e2a80cda02eb6421cc64d62fd3232febd22a9101
Lauryn05 <waruingilauryn05@gmail.com>
2023-02-23 16:47:10 -0800

    last

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,13 @@
  * print_numbers - Prints the numbers 0 to 9
  * No Return
  */
-int main(void)
+void print_numbers(void)
 {
 	int x;
 
-	for (x = 0; x <= 9; x++)
+	for (x = 48; x <= 58; x++)
 	{
-		_putchar(x + '0');
+		_putchar(x);
 	}
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,16 @@
 #include "main.h"
 /**
  * print_line - Draws straight line
- * @n number of lines
+ * @n: number of lines
  * No Return
  */
 void print_line(int n)
 {
 	int x;
 
-	if (n <= 0)
+	for (x = 0; x < n; x++)
 	{
-		_putchar('\n');
+		_putchar(95);
 	}
-	else
-	{
-		for (x = 0; x < n; x++)
-		{
-			_putchar(95);
-		}
 	_putchar('\n');
-	}
 }
