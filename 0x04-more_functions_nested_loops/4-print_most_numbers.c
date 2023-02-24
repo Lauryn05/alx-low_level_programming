5deec562c5e9f1607e38a9f616c4222598d21821
Lauryn05 <waruingilauryn05@gmail.com>
2023-02-23 16:52:46 -0800

    numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,9 +8,9 @@ void print_most_numbers(void)
 {
 	int x = 0;
 
-	for (x = 45; x < 58; x++)
+	for (x = 48; x < 58; x++)
 	{
-		if (x != 2 && x != 4)
+		if (x != 50 && x != 52)
 		{
 			_putchar(x);
 		}
