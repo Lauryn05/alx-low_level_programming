754360e3116223f152ac5fca7b69035dde6bcc83
Lauryn05 <waruingilauryn05@gmail.com>
2023-02-27 21:30:25 +0300

    initial

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+include "main.h"
+/**
+ * _strlen - Returns length of a string
+ * @str: string to be measured
+ * Return: length of @str
+ */
+size_t _strlen(const char *str)
+{
+	size_t length = 0;
+
+	while (*str++)
+		length++;
+	return (length);
+}
