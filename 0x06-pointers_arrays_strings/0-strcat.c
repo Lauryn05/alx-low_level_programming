9299dd950905f8d84996149a95bb0ff3153abfd6
Lauryn05 <waruingilauryn05@gmail.com>
2023-03-02 10:07:04 -0800

    initial

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - Concecrates 2 strings
+ * @dest: destination string
+ * @src: source string
+ * Pointer to resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dlen = 0, i;
+
+	while (dest[dlen])
+	{
+		dlen++;	
+	}
+	for (i = 0; src[1] != 0; i++)
+	{
+		dest[dlen] = src[i];
+		dlen++;
+	}
+	dest[dlen] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: string to be appended upon
+ * @src: string to be appended to dest
+ * @n: number of bytes from src to be appended to dest
+ * Return: pointer to resulting string dest
+ */
+char *_strcat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
+}
