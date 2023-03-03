965d8819bb370992605c58e051d5088a690af651
Lauryn05 <waruingilauryn05@gmail.com>
2023-03-02 18:25:00 -0800

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,24 +1,26 @@
 #include "main.h"
 /**
  * leet - encode into 1337speak
- * @n: input value
- * Return: n
+ * @s: input value
+ * Return: s
  */
-char *leet(char *n)
+char *leet(char *s)
 {
-	int i, j;
-	char s1[] = "aAeEoOtTlL";
-	char s2[] = "4433007711";
+	int count = 0, j;
+	int s1[] = {97, 101, 111, 116, 108};
+	int s2[] = {65, 69, 79, 84, 76};
+	int nos[] = {52, 51, 48, 55, 49};
 
-	for (i = 0; n[1] != '\0'; i++)
+	while (*(s + count) != '\0')
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j < 5; j++)
 		{
-			if (n[i] == s1[j])
+			if (*(s + count) == s1[j] || *(s + count) == s2[j])
 			{
-				n[i] = s2[j];
+				*(s + count) = nos[j];
 			}
 		}
+		count++;
 	}
-	return (n);
+	return (s);
 }
