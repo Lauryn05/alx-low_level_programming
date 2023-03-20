6cc0e403aeca1d28873a5f71f9ed15805ff2c0db
Lauryn05 <waruingilauryn05@gmail.com>
2023-03-20 10:48:39 -0700

    absolute

---
+++ 0x0D-preprocessor/3-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "3-function_like_macro.h"
+#include "3-function_like_macro.h"
+
+/**
+ * main - check code 
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	a = ABS(-98) * 10;
+	b = ABS(98) * 10;
+	printf("%d, %d\n", a, b);
+	return (0);
+}
+
+
