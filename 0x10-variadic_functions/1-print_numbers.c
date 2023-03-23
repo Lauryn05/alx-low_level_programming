3dace73a3800f444a8999abecb6d8b028e45113b
Lauryn05 <waruingilauryn05@gmail.com>
2023-03-23 12:13:13 -0700

    print int

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints numbers then new line
+ * @separator: string printed between numbers
+ * @n: number of integers to be printed
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int a;
+	va_list list;
+
+	va_start(list, n);
+	for (a = 0; a < n; a++)
+	{
+		if (!separator)
+			printf("%d", va_arg(list, int));
+		else if (separator && a == 0)
+			printf("%d", va_arg(list, int));
+		else
+			printf("%s%d", separator, va_arg(list, int));
+	}
+	va_end(list);
+	printf("\n");
+}
