2d9b571476efacf9bcd2814fc8e28559c08e3f31
Lauryn05 <waruingilauryn05@gmail.com>
2023-03-30 12:40:07 -0700

    first

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,14 +6,14 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t ele;
+	size_t ele = 0;
 
 	while (h)
 	{
 		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		ele++;
 	}
