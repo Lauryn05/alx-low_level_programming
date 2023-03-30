d395dd7ceff66473f3dd48a237ba7d4a6cee6b7a
Lauryn05 <waruingilauryn05@gmail.com>
2023-03-30 14:51:13 -0700

    third

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,17 +8,16 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	unsigned int len;
-
-	while (str[len])
-		len++;
+	size_t len;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
+	for (len = 0; str[len]; len++)
+		;
 	new->len = len;
-	new->next = *head;
-	*head = new;
+	new->next = (*head);
+	(*head) = new;
 	return (*head);
 }
