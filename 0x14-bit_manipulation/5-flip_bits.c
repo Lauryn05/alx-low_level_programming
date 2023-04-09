11ef4403a4ef26bfd482c2b41995be0f084a2493
Lauryn05 <waruingilauryn05@gmail.com>
2023-04-09 13:58:49 -0700

    betty

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -3,7 +3,8 @@
  * flip_bits - flips bits to get from one number to another
  * @n: number to convert from
  * @m: number to convert to
- * Return: number of bits you would need to flip to get from one number to another
+ * Return: number of bits you would need to
+ * flip to get from one number to another
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
