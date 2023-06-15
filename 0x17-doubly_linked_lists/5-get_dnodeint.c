56f843981b45cdf54368ced1b9f671b00b7f88e8
Lauryn05 <waruingilauryn05@gmail.com>
2023-06-15 14:26:17 -0700

    debug

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -21,7 +21,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		if (i == index)
 			break;
 		head = head->next;
-		i++
+		i++;
 	}
 	return (head);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,5 @@
-#define LISTS_H
 #ifndef LISTS_H
-
+#define LISTS_H
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -30,5 +29,4 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
-
 #endif
